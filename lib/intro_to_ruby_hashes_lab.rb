a2f91544ea54d2b73ba2bdcd9ad5eaf3f7e54f20
David Lee <david.lee.sl2634@yale.edu>
2020-05-13 23:03:13 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -3,9 +3,8 @@ def new_hash
 end
 
 def my_hash
-  my_hash = {:majorleague = "MLB"}
+  my_hash = {:majorleague => "MLB"}
 end
-my_hash[:majorleague]
 
 def pioneer
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
